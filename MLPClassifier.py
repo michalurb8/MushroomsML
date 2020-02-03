272bd04bfcd7ebd76ee5c6dc863e08b5b53e3434
michalurb8 <michalurb8@wp.pl>
2020-02-03 01:05:17 +0100

    Change plotting, need to fix it further, maybe types

--- MLPClassifier.py
+++ MLPClassifier.py
@@ -5,11 +5,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import LabelEncoder
 
-import warnings
-warnings.filterwarnings("ignore")
-
 grzybki = pan.read_csv("agaricus-lepiota.data")
-#grzybki = pan.read_csv("probka")
+#grzybki = pan.read_csv("sample.data")
 
 X = grzybki.drop(['0'], axis=1)
 Y = grzybki.take([0], axis=1)
@@ -18,19 +15,18 @@ Y = (Y.apply(LabelEncoder().fit_transform))
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
 
-#print(X_train); print(); print(X_test); print(); print(Y_train); print(); print(Y_test); print()
-
-network = MLPClassifier(hidden_layer_sizes=(100,100,100), max_iter = 50, solver='sgd')
+network = MLPClassifier(hidden_layer_sizes=(3,3), max_iter = 50, solver='sgd')
 network.fit(X_train, Y_train)
 Y_predicted = network.predict(X_test)
 
-Y_predicted=Y_predicted[:, np.newaxis]
+Y_predicted = Y_predicted[:, np.newaxis]
 
 error = np.mean((Y_predicted - Y_test)**2)
 print(error)
 
-f, axarr = plt.subplots()
-axarr.scatter(X_test.take([0], axis=1), Y_test,  color='red')
-axarr.plot(X_test, Y_predicted, color='blue', linewidth=1)
-        
-#plt.show()
+f, axarr = plt.subplots(11, 2)
+for i in range(2):
+    for j in range(11):
+        axarr[j, i].scatter(X_test.take([11*i+j], axis=1), Y_test,  color='red')
+        axarr[j, i].plot(X_test.take([11*i+j], axis=1), Y_predicted, color='blue', linewidth=1)
+plt.show()
--- Regression.py
+++ Regression.py
@@ -2,8 +2,8 @@ import pandas as pan
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from sklearn import linear_model
+from sklearn.preprocessing import LabelEncoder
 
 grzybki = pan.read_csv("agaricus-lepiota.data")
 
@@ -14,18 +14,16 @@ Y = (Y.apply(LabelEncoder().fit_transform))
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
 
-#print(X_train); print(); print(X_test); print(); print(Y_train); print(); print(Y_test); print()
-
 regr = linear_model.LinearRegression()
 regr.fit(X_train, Y_train)
 Y_predicted = regr.predict(X_test)
-print(Y_test.shape)
-print(Y_predicted.shape)
 
 error = np.mean((Y_predicted - Y_test)**2)
 print(error)
 
-#f, axarr = plt.subplots()
-#axarr.scatter(X_test.take([0], axis=1), Y_test,  color='red')
-#axarr.plot(X_test, regr.predict(X_test), color='blue', linewidth=1)
-#plt.show()
+f, axarr = plt.subplots(11, 2)
+for i in range(2):
+    for j in range(11):
+        axarr[j, i].scatter(X_test.take([11*i+j], axis=1), Y_test,  color='red')
+        axarr[j, i].plot(X_test.take([11*i+j], axis=1), Y_predicted, color='blue', linewidth=1)
+plt.show()
